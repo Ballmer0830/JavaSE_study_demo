05b6b10aeee6cc1ba245c3d37fba6c64b3e444c4
junbao3 <junbao3@iflytek.com>
2024-08-13 16:41:06 +0800

    if示例 switch示例 for示例

---
+++ src/com/u2/ch6/Demo_02.java
@@ -0,0 +1,21 @@
+package com.u2.ch6;
+
+/**
+ * ClassName: Demo_02
+ * Package: com.u2.ch6
+ * Description:
+ *
+ * @author junbao3
+ * Create 2024-08-13 16:33
+ * @version 1.0
+ */
+public class Demo_02 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
