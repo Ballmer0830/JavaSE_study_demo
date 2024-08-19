47341d9098d2a4811e21a9c90b4f93785abdb5c5
junbao3 <junbao3@iflytek.com>
2024-08-19 20:26:46 +0800

    方法

---
+++ src/com/u3/ch2/PersonV2.java
@@ -0,0 +1,20 @@
+package com.u3.ch2;
+
+/**
+ * ClassName: PersonV2
+ * Package: com.u3.ch2
+ * Description:
+ *
+ * @author junbao3
+ * Create 2024-08-19 17:23
+ * @version 1.0
+ */
+public class PersonV2 {
+    String name;
+    Integer age;
+
+    public PersonV2(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+}
